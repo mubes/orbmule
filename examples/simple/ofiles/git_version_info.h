294ff83111403cc4e1b77611f0b74ae89bb27d81
Dave Marples <dave@marples.net>
2023-03-09 15:30:53 +0000

    Make build artifact pathnames relative

--- examples/simple/ofiles/git_version_info.h
+++ examples/simple/ofiles/git_version_info.h
@@ -1,4 +1,4 @@
-#define GIT_HASH 0x1fee2965
+#define GIT_HASH 0x35c8346e
 #define GIT_BRANCH "main"
-#define BUILD_DATE "2023-03-07 11:50:51+0000"
+#define BUILD_DATE "2023-03-09 15:27:43+0000"
 #define GIT_DIRTY  1
